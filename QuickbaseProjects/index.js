79e50a5171ecb80a365d76fb20a8c33c0e4949e8
EL <elicloudh@gmail.com>
2023-10-16 19:13:26 -0700

    fix: quick base projects

--- QuickbaseProjects/index.js
+++ QuickbaseProjects/index.js
@@ -59,27 +59,70 @@ const executeSql = (connection, sql) => {
     });
 };
 
-const formatLabel = (label) =>
-    label
+const formatLabel = (label) => {
+    const newLabel = label
         .match(
             /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
         )
         .map((s) => s.toUpperCase())
         .join('_');
 
+    return newLabel.match(/^\d/) || newLabel === 'CURRENT_USER'
+        ? `_${newLabel}`
+        : newLabel;
+};
+
 const fetchQuickbaseProjects = async function () {
+    const tableId = 'br9kwm8na';
+    const hostName = 'kin.quickbase.com';
+    const authToken = `QB-USER-TOKEN b8gnqt_p3bs_0_di3bii2dt3jjyjcrvarkfbw53yrk`;
+
+    const headers = {
+        'QB-Realm-Hostname': hostName,
+        Authorization: authToken,
+    };
+
+    const { data: fields } = await axios.get(
+        'https://api.quickbase.com/v1/fields',
+        {
+            params: {
+                tableId,
+            },
+            headers,
+        }
+    );
+
     const { data } = await axios.post(
         'https://api.quickbase.com/v1/records/query',
         {
-            from: 'br9kwm8na',
+            from: tableId,
         },
         {
-            headers: {
-                'QB-Realm-Hostname': 'kin.quickbase.com',
-                Authorization: `QB-USER-TOKEN b8gnqt_p3bs_0_di3bii2dt3jjyjcrvarkfbw53yrk`,
-            },
+            headers,
         }
     );
+    const newFields = fields.map((f) => ({
+        id: f.id,
+        uniqueLabel: formatLabel(f.label),
+    }));
+
+    for (const field of newFields) {
+        const sameFields = newFields.filter(
+            (f) => f.uniqueLabel === field.uniqueLabel
+        );
+
+        if (sameFields.length === 1) {
+            continue;
+        }
+        field.index = sameFields.findIndex((f) => f.id === field.id) + 1;
+    }
+
+    for (const field of newFields) {
+        if (!field.index) {
+            continue;
+        }
+        field.uniqueLabel = field.uniqueLabel + '__' + field.index;
+    }
 
     const records = data.data.map((item) => {
         const r = {};
@@ -87,24 +130,31 @@ const fetchQuickbaseProjects = async function () {
         for (const key of Object.keys(item)) {
             const id = +key;
             const value = item[key].value;
-            const field = data.fields.find((f) => f.id === id);
-            r[formatLabel(field.label)] = value;
+            const field = newFields.find((f) => f.id === id);
+            r[field.uniqueLabel] = value;
         }
         return r;
     });
 
-    const columns = data.fields.map((f) => formatLabel(f.label));
+    const columns = newFields.map((f) => f.uniqueLabel);
 
     const connection = await initSnowflakeConnection();
 
     console.log('Creating table');
-    // create table if not exists with columns
-    await executeSql(
-        connection,
-        `CREATE OR REPLACE TABLE QUICKBASE_PROJECTS (${columns
-            .map((c) => `${c} VARCHAR`)
-            .join(', ')})`
-    );
+
+    try {
+        // create table if not exists with columns
+        await executeSql(
+            connection,
+            `CREATE OR REPLACE TABLE QUICKBASE_PROJECTS (${columns
+                .map((c) => `${c} VARCHAR`)
+                .join(', ')})`
+        );
+    } catch (err) {
+        console.error('Failed to create table');
+        console.error(err);
+        return;
+    }
 
     console.log('Inserting rows');
 
@@ -129,12 +179,17 @@ const fetchQuickbaseProjects = async function () {
         )
         .join(', ');
 
-    await executeSql(
-        connection,
-        `INSERT INTO QUICKBASE_PROJECTS (${columns.join(
-            ', '
-        )}) VALUES ${values}`
-    );
+    try {
+        await executeSql(
+            connection,
+            `INSERT INTO QUICKBASE_PROJECTS (${columns.join(
+                ', '
+            )}) VALUES ${values}`
+        );
+    } catch (err) {
+        console.error('Failed to insert records');
+        console.error(err);
+    }
 };
 
 module.exports = async function (context, myTimer) {
